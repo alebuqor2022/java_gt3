390a7e21d8d472f33fbe944f63e571bc89369be3
alebuqor2022 <97527361+alebuqor2022@users.noreply.github.com>
2023-03-13 07:52:13 -0300

    Add files via upload

---
+++ locations_data.sql
@@ -0,0 +1,5 @@
+INSERT INTO indragt3.locations(LOCATION_ID, STREET_ADDRESS, POSTAL_CODE, CITY, STATE_PROVINCE, COUNTRY_ID) VALUES ('1400', '2014 Jabberwocky Rd', '26192', 'Southlake', 'Texas', 'US');
+INSERT INTO indragt3.locations(LOCATION_ID, STREET_ADDRESS, POSTAL_CODE, CITY, STATE_PROVINCE, COUNTRY_ID) VALUES ('1500', '2011 Interiors Blvd', '99236', 'South San Francisco', 'California', 'US');
+INSERT INTO indragt3.locations(LOCATION_ID, STREET_ADDRESS, POSTAL_CODE, CITY, STATE_PROVINCE, COUNTRY_ID) VALUES ('1700', '2004 Charade Rd', '98199', 'Seattle', 'Washington', 'US');
+INSERT INTO indragt3.locations(LOCATION_ID, STREET_ADDRESS, POSTAL_CODE, CITY, STATE_PROVINCE, COUNTRY_ID) VALUES ('1800', '460 Bloor St. W.', 'ON M5S 1X8', 'Toronto', 'Ontario', 'CA');
+INSERT INTO indragt3.locations(LOCATION_ID, STREET_ADDRESS, POSTAL_CODE, CITY, STATE_PROVINCE, COUNTRY_ID) VALUES ('2500', 'Magdalen Centre, The Oxford Science Park', 'OX9 9ZB', 'Oxford', 'Oxford', 'UK');
