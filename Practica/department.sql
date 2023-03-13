0a1ce7a7564d65f2e03bf3d7690a2cdeee062372
alebuqor2022 <97527361+alebuqor2022@users.noreply.github.com>
2023-03-13 08:49:12 -0300

    Add files via upload

---
+++ Practica/department.sql
@@ -0,0 +1,13 @@
+CREATE TABLE indragt3.department (
+  department_id INT NOT NULL,
+  location_id INT NOT NULL,
+  PRIMARY KEY (department_id, location_id));
+
+INSERT INTO indragt3.department (department_id, location_id) VALUES ('90', '1400');
+INSERT INTO indragt3.department (department_id, location_id) VALUES ('60', '1500');
+INSERT INTO indragt3.department (department_id, location_id) VALUES ('50', '1700');
+INSERT INTO indragt3.department (department_id, location_id) VALUES ('80', '1800');
+INSERT INTO indragt3.department (department_id, location_id) VALUES ('10', '2500');
+INSERT INTO indragt3.department (department_id, location_id) VALUES ('20', '1400');
+INSERT INTO indragt3.department (department_id, location_id) VALUES ('110', '1700');
+
